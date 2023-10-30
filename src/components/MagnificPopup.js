03ed64a42dabd6deec852c039e27bfe8c69709b4
Mirxa Hassan <128700193+Hassanbaig36@users.noreply.github.com>
2023-10-30 07:57:52 -0700

    Update MagnificPopup.js

--- src/components/MagnificPopup.js
+++ src/components/MagnificPopup.js
@@ -1,10 +1,52 @@
-
-import Script from 'next/script';
+import React, { useEffect } from 'react';
+import Head from 'next/head';
 
 const MagnificPopup = () => {
+  useEffect(() => {
+    $(document).ready(function () {
+      $('.video-play').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false,
+        iframe: {
+          patterns: {
+            youtube: {
+              index: 'youtube.com/',
+              id: 'v=',
+              src: 'https://www.youtube.com/embed/Y4UwssndcPA',
+            },
+          },
+          srcAction: 'iframe_src',
+        },
+      });
+    });
+  }, []);
+
   return (
     <>
-    
+      <Head>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdn.usebootstrap.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        />
+
+        <link
+          href="https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro@663d232/css/all.css"
+          rel="stylesheet"
+          type="text/css"
+        />
+
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.css"
+        />
+      </Head>
+
       <div className="magnific-wrapper">
         <div className="container">
           <div className="row justify-content-center">
@@ -27,35 +69,6 @@ const MagnificPopup = () => {
           </div>
         </div>
       </div>
-      <Script>
-        {`
-          $(document).ready(function() {
-            console.log('Before magnificPopup');
-
-            // Call the magnificPopup method directly on $('.video-play')
-            $('.video-play').magnificPopup({
-              type: 'iframe',
-              disableOn: 700,
-              mainClass: 'mfp-fade',
-              removalDelay: 160,
-              preloader: false,
-              fixedContentPos: false,
-              iframe: {
-                patterns: {
-                  youtube: {
-                    index: 'youtube.com/',
-                    id: 'v=1',
-                    src: 'https://www.youtube.com/embed/Y4UwssndcPA',
-                  },
-                },
-                srcAction: 'iframe_src',
-              },
-            });
-
-            console.log('After magnificPopup');
-          });
-        `}
-      </Script>
     </>
   );
 };
