5250c5b6566f5e4c08435811a44014247a4f7fa7
Mirxa Hassan <128700193+Hassanbaig36@users.noreply.github.com>
2023-10-30 08:06:52 -0700

    Update DigitalM.js

--- src/components/DigitalM.js
+++ src/components/DigitalM.js
@@ -14,7 +14,7 @@ import { digitalMarketing } from "@/data/sidebarPageContainer";
 const { navItems, } = headerData;
 
 function DigitalM() {
-    const { logo11 } = headerData; 
+    const { logo7 } = headerData; 
     useEffect(() => {
         const paperMenu = {
             $window: document.querySelector('#paper-window'),
@@ -95,7 +95,7 @@ function DigitalM() {
                 <div id="paper-front">
                     <Link href="/" className='nav-logoa' passHref>
 
-                    <Image className="nav-logo1" src={logo11.src} alt="" />
+                    <Image className="nav-logo1" src={logo7.src} alt="" />
 
                     </Link>
                     <div className="hamburger">
